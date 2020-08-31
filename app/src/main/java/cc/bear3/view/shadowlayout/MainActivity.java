b4e0717679a26aefd18feb18ff59f8fbc9175523
TT <tt@thecover.co>
2020-08-31 17:48:44 +0800

    commit

---
+++ app/src/main/java/cc/bear3/view/shadowlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package cc.bear3.view.shadowlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
