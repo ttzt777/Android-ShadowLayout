87808cd759b02c73c617b396ad632ee6aab36d73
TT <tt@thecover.co>
2020-08-31 18:15:06 +0800

    commit

--- app/src/main/java/cc/bear3/view/shadowlayout/MainActivity.java
+++ app/src/main/java/cc/bear3/view/shadowlayout/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import cc.bear3.view.shadowlayout.layout.IQMUILayout;
+import cc.bear3.view.shadowlayout.layout.QMUILinearLayout;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        QMUILinearLayout layout = findViewById(R.id.layout);
+
+        layout.setRadiusAndShadow(20, IQMUILayout.HIDE_RADIUS_SIDE_NONE, 50, 0xFFFF0000, 1);
     }
 }
---
+++ shadowlayout/src/main/java/cc/bear3/view/shadowlayout/helper/QMUIResHelper.java
@@ -0,0 +1,61 @@
+/*
+ * Tencent is pleased to support the open source community by making QMUI_Android available.
+ *
+ * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the MIT License (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ * http://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cc.bear3.view.shadowlayout.helper;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.TypedValue;
+
+/**
+ * @author cginechen
+ * @date 2016-09-22
+ */
+public class QMUIResHelper {
+    private static TypedValue sTmpValue;
+
+    public static float getAttrFloatValue(Context context, int attr) {
+        return getAttrFloatValue(context.getTheme(), attr);
+    }
+
+    public static float getAttrFloatValue(Resources.Theme theme, int attr) {
+        if (sTmpValue == null) {
+            sTmpValue = new TypedValue();
+        }
+        if (!theme.resolveAttribute(attr, sTmpValue, true)) {
+            return 0;
+        }
+        return sTmpValue.getFloat();
+    }
+
+    public static int getAttrColor(Context context, int attrRes) {
+        return getAttrColor(context.getTheme(), attrRes);
+    }
+
+    public static int getAttrColor(Resources.Theme theme, int attr) {
+        if (sTmpValue == null) {
+            sTmpValue = new TypedValue();
+        }
+        if (!theme.resolveAttribute(attr, sTmpValue, true)) {
+            return 0;
+        }
+        if (sTmpValue.type == TypedValue.TYPE_ATTRIBUTE) {
+            return getAttrColor(theme, sTmpValue.data);
+        }
+        return sTmpValue.data;
+    }
+
+}
